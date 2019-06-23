0bd284ce6af2b7d686981ec9d57cb4c5dc764ef1
Janis Daniel Dähne <janisdd5566@gmail.com>
2019-06-23 23:57:46 +0200

    - types fix

--- xpath.d.ts
+++ xpath.d.ts
@@ -11,19 +11,22 @@ export interface Xpath {
     // parse(expression: string)
     // select(expression: string, node?: Node): XPathSelect
     select1(expression: string, node?: Node): SelectedValue
-    evaluate(expression: string, contextNode: Node, resolver: XPathNSResolver, type: number, result: XPathResult): XPathResult
+    evaluate(expression: string, contextNode: Node, resolver: XPathNSResolver | null, type: number, result: XPathResult | null): XPathResult
     useNamespaces(namespaceMap: { [name: string]: string }): XPathSelect
 
-    ANY_TYPE: number
-    NUMBER_TYPE: number
-    STRING_TYPE: number
-    BOOLEAN_TYPE : number
-    UNORDERED_NODE_ITERATOR_TYPE: number
-    ORDERED_NODE_ITERATOR_TYPE : number
-    UNORDERED_NODE_SNAPSHOT_TYPE: number
-    ORDERED_NODE_SNAPSHOT_TYPE: number
-    ANY_UNORDERED_NODE_TYPE: number
-    FIRST_ORDERED_NODE_TYPE: number
+
+    XPathResult: {
+        ANY_TYPE: number
+        NUMBER_TYPE: number
+        STRING_TYPE: number
+        BOOLEAN_TYPE : number
+        UNORDERED_NODE_ITERATOR_TYPE: number
+        ORDERED_NODE_ITERATOR_TYPE : number
+        UNORDERED_NODE_SNAPSHOT_TYPE: number
+        ORDERED_NODE_SNAPSHOT_TYPE: number
+        ANY_UNORDERED_NODE_TYPE: number
+        FIRST_ORDERED_NODE_TYPE: number
+    }
 }
 
 export const xpath: Xpath
